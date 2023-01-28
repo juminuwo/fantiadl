1eef5395187b3e299c4ea5944965b509becd1109
Adrian Liu <adrianliu95@protonmail.com>
2023-01-28 13:45:42 +0000

    Use CSRF token on requests to /posts endpoint

--- auto.py
+++ auto.py
@@ -1,19 +1,32 @@
 import json
 from glob import glob
-import models
-from models import FantiaClub, POST_API, traceback
-from session_arg import session_arg, fanclub_urls  # type: ignore
 
-def download_remaining_posts(fanclub, downloader):
+from bs4 import BeautifulSoup
+
+import models
+from models import *
+from cmdl_values import fanclub_urls, output_path, session_arg  # type: ignore
+from models import POST_API, FantiaClub, traceback
+
+
+def download_remaining_posts(output_path, fanclub, downloader):
     """Get post ids and downloaded for fanclub and filters on downloader from
     folder lookup.
     """
     post_ids = downloader.fetch_fanclub_posts(fanclub)
-    response = downloader.session.get(POST_API.format(post_ids[0]))
+    post_html_response = downloader.session.get(POST_URL.format(post_ids[0]))
+    post_html_response.raise_for_status()
+    post_html = BeautifulSoup(post_html_response.text, "html.parser")
+    csrf_token = post_html.select_one("meta[name=\"csrf-token\"]")["content"]
+
+    response = downloader.session.get(POST_API.format(post_ids[0]), headers={
+        "X-CSRF-Token": csrf_token
+    })
     response.raise_for_status()
     post_json = json.loads(response.text)["post"]
+
     creator_name = post_json["fanclub"]["creator_name"]
-    downloaded_ids = glob('./{}/*'.format(creator_name))
+    downloaded_ids = glob('{}/{}/*'.format(output_path, creator_name))
     downloaded_ids = [id.split('/')[-1] for id in downloaded_ids]
     for post_id in post_ids:
         if post_id not in downloaded_ids:
@@ -35,6 +48,6 @@ if __name__ == '__main__':
         print('fanclub_url: {}'.format(fanclub_url))
         url_match = models.FANTIA_URL_RE.match(fanclub_url)
         url_groups = url_match.groups()
-        downloader = models.FantiaDownloader(session_arg=session_arg, quiet=False)
+        downloader = models.FantiaDownloader(session_arg=session_arg, quiet=False, directory=output_path)
         fanclub = models.FantiaClub(url_groups[1])
-        download_remaining_posts(fanclub, downloader)
+        download_remaining_posts(output_path, fanclub, downloader)
