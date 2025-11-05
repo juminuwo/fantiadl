420ee0727e79db0c5eaf55e38224e79d3ad6b033
Juminuwo <howismypielola@gmail.com>
2025-11-05 16:22:04 +0000

    Connect NordVPN during duration of auto_run.sh and then disconnect

--- auto_run.sh
+++ auto_run.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+# Connect to Japan for the script
+cd /mnt/c/
+/mnt/c/Windows/System32/cmd.exe /c "C:\Program Files\NordVPN\nordvpn.exe" nordvpn -c -g "Japan"
+
+# Run the Python script
 cd "$(dirname "$0")"
 source ~/.zshrc
 conda activate fantia-dler && /home/howis/miniconda3/envs/fantia-dler/bin/python /mnt/g/AV/fantiadl/auto.py
+
+# Disconnect NordVPN when done
+cd /mnt/c/
+/mnt/c/Windows/System32/cmd.exe /c "C:\Program Files\NordVPN\nordvpn.exe" nordvpn -d
